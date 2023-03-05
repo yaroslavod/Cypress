404afe1a0ee6c30bc9f82db0d3ca1033684a0c02
Yaroslav Odintsov <yaroslavod@gmail.com>
2023-03-05 02:11:53 -0500

    modified:   HotelSearchProject/cypress/e2e/travel-site-sanity-test.cy.js

--- HotelSearchProject/cypress/e2e/travel-site-sanity-test.cy.js
+++ HotelSearchProject/cypress/e2e/travel-site-sanity-test.cy.js
@@ -9,7 +9,7 @@ describe('The sanity test suite for travel site', () => {
 
     it('verify tabs of the page', () => {
         cy.visit('http://localhost:3000')
-        // knopka sywestvyet i vidna
+        // knopka sootwetsvyet i vidna
         cy.get('[data-cy="Hotels"]').should("be.visible")
         cy.get('[data-cy="Car"]').should("be.visible")
         cy.get('[data-cy="Flight"]').should("be.visible")
